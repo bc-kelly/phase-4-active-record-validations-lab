e583b95eb540fc60841602eee69107d85e6efdb2
Bridget Kelly <bckelly197@gmail.com>
2022-04-05 16:13:54 -0400

    commit tree 678cce8ada016985cfe57ce5b1f7a3498e88b143

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    
+    validate :clickbait
+
+    CLICKBAIT_PATS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait
+        if CLICKBAIT_PATS.none? { |pat| pat.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
+
+# "Won't Believe"
+# "Secret"
+# "Top [number]"
+# "Guess"
